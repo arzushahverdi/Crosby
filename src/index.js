d71ce2753445c4499e7bed57217a525e73bb487c
Arzu Shahverdiyeva <arzu.shahverdi@gmail.com>
2024-09-27 20:06:02 +0400

    Added Redux store as Provider to index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import App from "./App";
 import { CartProvider } from "react-use-cart";
 import { WishlistProvider } from "react-use-wishlist";
 import { FanFavoritesProvider } from "./context/fanfavoritesContext";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "./i18next";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <FanFavoritesProvider>
-    <WishlistProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </WishlistProvider>
-  </FanFavoritesProvider>
+  <Provider store={store}>
+    <FanFavoritesProvider>
+      <WishlistProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </WishlistProvider>
+    </FanFavoritesProvider>
+  </Provider>
 );
